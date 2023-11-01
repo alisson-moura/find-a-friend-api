e0e58999e5bdb6ae2255ea58b9bd01f2689ccd0f
alisson-moura <alisson.mo.moura@outlook.com.br>
2023-11-01 09:51:01 -0300

    test: register org

--- src/http/controllers/register-org/index.ts
+++ src/http/controllers/register-org/index.ts
@@ -2,19 +2,43 @@ import { type FastifyReply, type FastifyRequest } from 'fastify';
 import { type Org } from '../../../entities/Org';
 import { RegisterOrgUseCase } from '../../../use-cases/register-org';
 import { type OrgRepository } from '../../../repositories/org-repository';
-import { registerOrgBodySchema } from './register-org-schema';
+import { z } from 'zod';
+import { EmailAlreadyInUse } from '../../../use-cases/errors/email-already-use';
 
-export async function registerOrg (
-  request: FastifyRequest, reply: FastifyReply
-): Promise<FastifyReply> {
-  const body = registerOrgBodySchema.parse(request.body);
+export async function registerOrg (req: FastifyRequest, res: FastifyReply): Promise<FastifyReply> {
+  const body = requestBodyRegisterOrgSechema.parse(req.body);
   const useCase = new RegisterOrgUseCase(new OrgRepositoryFake());
-  await useCase.execute(body);
-  return await reply.status(200).send();
+  try {
+    await useCase.execute(body);
+  } catch (error) {
+    if (error instanceof EmailAlreadyInUse) {
+      return await res.status(409).send({ message: error.message });
+    }
+    throw error;
+  }
+  return await res.status(201).send();
 }
 
+export const requestBodyRegisterOrgSechema = z.object({
+  name: z.string().min(3),
+  owner: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(6),
+  phone: z.string().length(11),
+  address: z.object({
+    city: z.string().min(3),
+    state: z.string().length(2).toUpperCase(),
+    cep: z.string().min(9, 'Por favor informe um CEP válido (00000-000)'),
+    number: z.coerce.number().min(1, 'Por favor informe um número válido'),
+    street: z.string().min(1, 'Por favor ruma rua válida')
+  })
+});
+
 class OrgRepositoryFake implements OrgRepository {
-  findById: (orgId: string) => Promise<Org | null>;
+  async findById (orgId: string): Promise<Org | null> {
+    return null;
+  }
+
   async findByEmail (orgEmail: string): Promise<Org | null> {
     return null;
   }
---
+++ src/http/controllers/register-org/test.spec.ts
@@ -0,0 +1,42 @@
+import request from 'supertest';
+import { expect, describe, it, beforeAll, afterAll } from 'vitest';
+import app from '../..';
+import { type requestBodyRegisterOrgSechema } from '.';
+import { type z } from 'zod';
+
+const fakeRequest: z.infer<typeof requestBodyRegisterOrgSechema> = {
+  name: 'fakeNome do Usuário',
+  owner: 'fakeNome do Proprietário',
+  email: 'fake_email@example.com',
+  password: 'fakeSenhaSegura123',
+  phone: '12345678900',
+  address: {
+    cep: 'fake12345-678',
+    city: 'fakeCidade',
+    number: 123,
+    state: 'FS',
+    street: 'fakeNome da Rua'
+  }
+};
+describe('Register Org Controller', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+  it('deve ser possível registrar uma org', async () => {
+    const response = await request(app.server)
+      .post('/orgs')
+      .send(fakeRequest);
+    expect(response.statusCode).toEqual(201);
+  });
+  it('deve retornar o status 409 e uma mensagem caso o email já esteja em uso', async () => {
+    const response = await request(app.server)
+      .post('/orgs')
+      .send(fakeRequest);
+
+    expect(response.statusCode).toEqual(409);
+    expect(response.body.message).toEqual('O e-mail já está em uso por outra org');
+  });
+});
